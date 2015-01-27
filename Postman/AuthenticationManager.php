18d5324f6e21694d549e5fbde244fe18e51297fb
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 23:49:14 +0000

    AuthToken, RefreshToken, ExpiryTime are no longer stored in $options
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1076970 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Postman/AuthenticationManager.php
+++ Postman/AuthenticationManager.php
@@ -1,13 +1,10 @@
 <?php
-
-namespace Postman {
-
-	interface AuthenticationManager {
-		public function __construct(&$options);
+if (! interface_exists ( "PostmanAuthenticationManager" )) {
+	interface PostmanAuthenticationManager {
+		const AUTHORIZATION_IN_PROGRESS = 'AUTHORIZATION_IN_PROGRESS';
+		public function __construct($clientId, $clientSecret, PostmanAuthorizationToken &$authorizationToken);
 		public function isTokenExpired();
 		public function refreshToken();
 		public function authenticate($gmailAddress);
-		public function setAuthenticationToken(AuthenticationToken $authenticationToken);
-		public function getAuthenticationToken();
 	}
 }
