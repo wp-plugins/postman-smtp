e01ecc5a146b2f7e8a3e83cdd8ab5f28da6d9219
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 18:04:36 +0000

    Moved sending of the message into the try-catch block. The SmtpEngine should not be catching exceptions.
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1077707 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Postman/PostmanWpMail.php
+++ Postman/PostmanWpMail.php
@@ -10,37 +10,30 @@ if (! class_exists ( "PostmanWpMail" )) {
 			$this->logger = new PostmanLogger ();
 		}
 		public function send($to, $subject, $message, $headers = '', $attachments = array()) {
-			
-			// ensure the token is up-to-date
 			try {
-				$clientId = PostmanOptionUtil::getClientId($this->options);
-				$clientSecret = PostmanOptionUtil::getClientSecret($this->options);
-				$authorizationToken = new PostmanAuthorizationToken();
-				$authorizationToken->load();
-				$authenticationManager = new GmailAuthenticationManager($clientId, $clientSecret, $authorizationToken);
-				if ($authenticationManager->isTokenExpired () || true) {
+				// ensure the token is up-to-date
+				$clientId = PostmanOptionUtil::getClientId ( $this->options );
+				$clientSecret = PostmanOptionUtil::getClientSecret ( $this->options );
+				$authorizationToken = new PostmanAuthorizationToken ();
+				$authorizationToken->load ();
+				$authenticationManager = new GmailAuthenticationManager ( $clientId, $clientSecret, $authorizationToken );
+				if ($authenticationManager->isTokenExpired ()) {
 					$this->logger->debug ( 'Access Token has expired, attempting refresh' );
 					$authenticationManager->refreshToken ();
-					$authorizationToken->save();
+					$authorizationToken->save ();
 				}
+				// send the message
+				$this->engine = new PostmanOAuthSmtpEngine ( PostmanOptionUtil::getSenderEmail ( $this->options ), $authorizationToken->getAccessToken () );
+				$this->engine->setBodyText ( $message );
+				$this->engine->setSubject ( $subject );
+				$this->engine->addTo ( $to );
+				$this->engine->send ( PostmanOptionUtil::getHostname ( $this->options ), PostmanOptionUtil::getPort ( $this->options ) );
+				return true;
 			} catch ( Exception $e ) {
 				$this->exception = $e;
 				$this->logger->debug ( 'Error: ' . get_class ( $e ) . ' code=' . $e->getCode () . ' message=' . $e->getMessage () );
 				return false;
 			}
-			
-			// send the message
-			$this->engine = new PostmanOAuthSmtpEngine ( PostmanOptionUtil::getSenderEmail($this->options), $authorizationToken->getAccessToken() );
-			$this->engine->setBodyText ( $message );
-			$this->engine->setSubject ( $subject );
-			$this->engine->addTo ( $to );
-			
-			$result = $this->engine->send(PostmanOptionUtil::getHostname($this->options), PostmanOptionUtil::getPort($this->options));
-			if (! $result) {
-				$this->exception = $this->engine->getException ();
-			}
-			
-			return $result;
 		}
 		public function getException() {
 			return $this->exception;
