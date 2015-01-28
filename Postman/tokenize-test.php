a13cabcdf3bd394788fd2aa11f1553585b6634c3
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 18:55:56 +0000

    Fix Fatal error: Call to undefined function str_getcsv() in /free/home/kaorwnet/html/wp-content/plugins/postman-smtp/Postman/OAuthSmtpEngine.php on line 111
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1077729 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Postman/tokenize-test.php
@@ -0,0 +1,29 @@
+<?php
+$multipleEmails = 'jason@hendriks.ca, blake@tvwp.ca';
+display ( str_getcsv ( $multipleEmails ) );
+display ( stringGetCsvAlternate ( $multipleEmails ) );
+function display($t) {
+	foreach ( $t as $k => $v ) {
+		if (strpos ( $v, ',' ) !== false) {
+			$t [$k] = '"' . str_replace ( ' <', '" <', $v );
+		}
+		$email = trim ( $t [$k] );
+		print $email . "\n";
+	}
+}
+
+/**
+ * Using fgetscv (PHP 4) as a work-around for str_getcsv (PHP 5.3)
+ * @param unknown $string
+ * @return multitype:
+ */
+function stringGetCsvAlternate($string) {
+	$fh = fopen ( 'php://temp', 'r+' );
+	fwrite ( $fh, $string );
+	rewind ( $fh );
+	
+	$row = fgetcsv ( $fh );
+	
+	fclose ( $fh );
+	return $row;
+}
