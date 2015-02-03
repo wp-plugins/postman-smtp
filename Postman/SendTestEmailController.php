e90b508ab9c5707fd8006a90bd975c8adba1357b
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-03 23:36:03 +0000

    Allow sender email overrides
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1081908 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Postman/SendTestEmailController.php
+++ Postman/SendTestEmailController.php
@@ -24,16 +24,17 @@ if (! class_exists ( "PostmanSendTestEmailController" )) {
 			assert ( ! empty ( $recipient ) );
 			assert ( ! empty ( $messageHandler ) );
 			
+			$headers = array();
 			// $recipient = 'Kevin.Brine@pppg.com, Robert.Thomas@pppg.com, "Guice, Doug" <Doug.Guice@pppg.com>';
 			$subject = PostmanSendTestEmailController::SUBJECT;
 			// Lines in email are terminated by CRLF ("\r\n") according to RFC2821
 			// Englsih - Mandarin - French - Hindi - Spanish - Arabic - Portuguese - Russian - Bengali - Japanese - Punjabi
 			$message = 'Hello! - 你好 - Bonjour! - नमस्ते - ¡Hola! - السلام عليكم - Olá - Привет! - নমস্কার - 今日は - ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ।';
 			$message .= PostmanSendTestEmailController::EOL . PostmanSendTestEmailController::EOL . 'Sent by Postman v' . POSTMAN_PLUGIN_VERSION . ' - https://wordpress.org/plugins/postman-smtp/';
-			$headers = array (
-					'Content-Type: text/html;',
-					'From: Brian <brian@postman.com>' 
-			);
+// 			$headers = array (
+// 					'Content-Type: text/html;',
+// 					'From: Brian <brian@postman.com>' 
+// 			);
 			
 			// send through wp_mail
 			$this->logger->debug ( 'Sending Test email' );
