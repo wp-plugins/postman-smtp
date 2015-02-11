92e9054141313eab12cf79d27e74e8eabd68fdff
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 13:04:52 +0000

    Try and figure out why opening ports doesn't work consistently on openshift
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1087113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- port-test.php
+++ port-test.php
@@ -4,8 +4,10 @@ test ( 'smtp.live.com', 25 );
 test ( 'smtp.live.com', 465 );
 test ( 'smtp.live.com', 587 );
 function test($host, $port) {
+	$time = time();
 	$p = new PostmanPortTest ();
 	$success = $p->testSmtpPorts ( 'smtp.live.com', $port, 20 );
 	$message = $p->getErrorMessage ();
-	print "\n$host:$port success=$success message=$message\n";
+	$time = time() - $time;
+	print "\n$host:$port time=$time success=$success message=$message\n";
 }
