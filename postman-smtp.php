4d678434fec99a0aca76a746643f166c03b6cba7
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 02:33:45 +0000

    README
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1109173 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postman-smtp.php
+++ postman-smtp.php
@@ -13,8 +13,9 @@
  */
 
 // ideas for future versions of the plugin
-// -- add WPMU functionality
+// -- add WPMU functionality. ideas: allow network setup for network emails. allow network admin to choose whether subdomains may override with their own settings. subdomains may override with their own settings.
 // -- add timeout setting for connectivity test
+// -- change SMTP discovery to fallback to A record if there is no MX record
 // -- if a test message succeeds, capture the domain of the primary mx, smtp server hostname and port, auth type, encryption type into a database (with the user's permission)
 // -- write a plugin to capture the above information
 // -- send mail in the background using ajax - the single mail thread can block the PHP server for quite some time
