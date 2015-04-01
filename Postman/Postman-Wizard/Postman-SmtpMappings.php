ffa4743a3c20e7e840aa0a991f070fadc5075310
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 05:34:49 +0000

    Updated domain mappings
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1125065 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Postman/Postman-Wizard/Postman-SmtpMappings.php
+++ Postman/Postman-Wizard/Postman-SmtpMappings.php
@@ -5,12 +5,33 @@ if (! class_exists ( 'SmtpMapping' )) {
 		private $emailDomain = array (
 				'gmail.com' => 'smtp.gmail.com',
 				'hotmail.com' => 'smtp.live.com',
-				'outlook.com' => 'smtp.live.com',
-				'yahoo.co.uk' => 'smtp.mail.yahoo.co.uk',
-				'yahoo.com.au' => 'smtp.mail.yahoo.com.au',
-				'yahoo.com' => 'smtp.mail.yahoo.com',
 				'icloud.com' => 'smtp.mail.me.com',
-				'mail.com' => 'smtp.mail.com' 
+				'mail.com' => 'smtp.mail.com',
+				'rocketmail.com' => 'plus.smtp.mail.yahoo.com',
+				'yahoo.ca' => 'smtp.mail.yahoo.ca',
+				'yahoo.co.id' => 'smtp.mail.yahoo.co.id',
+				'yahoo.co.in' => 'smtp.mail.yahoo.co.in',
+				'yahoo.co.kr' => 'smtp.mail.yahoo.com',
+				'yahoo.com' => 'smtp.mail.yahoo.com',
+				'yahoo.com.ar' => 'smtp.mail.yahoo.com.ar',
+				'yahoo.com.au' => 'smtp.mail.yahoo.com.au',
+				'yahoo.com.br' => 'smtp.mail.yahoo.com.br',
+				'yahoo.com.cn' => 'smtp.mail.yahoo.com.cn',
+				'yahoo.com.hk' => 'smtp.mail.yahoo.com.hk',
+				'yahoo.com.mx' => 'smtp.mail.yahoo.com',
+				'yahoo.com.my' => 'smtp.mail.yahoo.com.my',
+				'yahoo.com.ph' => 'smtp.mail.yahoo.com.ph',
+				'yahoo.com.sg' => 'smtp.mail.yahoo.com.sg',
+				'yahoo.com.tw' => 'smtp.mail.yahoo.com.tw',
+				'yahoo.com.vn' => 'smtp.mail.yahoo.com.vn',
+				'yahoo.co.nz' => 'smtp.mail.yahoo.com.au',
+				'yahoo.co.th' => 'smtp.mail.yahoo.co.th',
+				'yahoo.co.uk' => 'smtp.mail.yahoo.co.uk',
+				'yahoo.de' => 'smtp.mail.yahoo.de',
+				'yahoo.es' => 'smtp.correo.yahoo.es',
+				'yahoo.fr' => 'smtp.mail.yahoo.fr',
+				'yahoo.ie' => 'smtp.mail.yahoo.co.uk',
+				'yahoo.it' => 'smtp.mail.yahoo.it' 
 		);
 		// if an email's mx is in this domain array, it is a known smtp server (dns lookup)
 		// useful for custom domains that map to a mail service
@@ -18,8 +39,13 @@ if (! class_exists ( 'SmtpMapping' )) {
 				'google.com' => 'smtp.gmail.com',
 				'icloud.com' => 'smtp.mail.me.com',
 				'hotmail.com' => 'smtp.live.com',
+				'mx-eu.mail.am0.yahoodns.net' => 'smtp.mail.yahoo.com',
+				'mail.protection.outlook.com' => 'smtp.office365.com',
+				'biz.mail.am0.yahoodns.net' => 'smtp.bizmail.yahoo.com',
 				'hushmail.com' => 'smtp.hushmail.com',
-				'secureserver.net' => 'smtp.secureserver.net'
+				'gmx.net' => 'mail.gmx.com',
+				'mandrillapp.com' => 'smtp.mandrillapp.com',
+				'secureserver.net' => 'relay-hosting.secureserver.net' 
 		);
 		public function getSmtpFromEmail($email) {
 			$hostname = substr ( strrchr ( $email, "@" ), 1 );
--- test-smtp-discovery.php
+++ test-smtp-discovery.php
@@ -2,37 +2,71 @@
 require_once 'Postman/postman-common-functions.php';
 require_once 'Postman/Postman-Wizard/Postman-SmtpDiscovery.php';
 function test() {
-	check ( 'test@hendriks.ca' );
-	check ( 'test@hotmail.com' );
-	check ( 'test@live.com' );
-	check ( 'test@outlook.com' );
-	check ( 'test@office365.com' );
-	check ( 'test@gmail.com' );
-	check ( 'test@yahoo.com.co' );
-	check ( 'test@yahoo.com' );
-	check ( 'test@hendriksandcregg.com' );
-	check ( 'test@yahoo.co.uk' );
-	check ( 'test@yahoo.com.au' );
-	check ( 'test@ibm.com' );
-	check ( 'test@sdlkfjsdl.co.uk' );
-	check ( 'test@sdlkfjsdl.org' );
-	check ( 'test@sdlkfjsdl.gov' );
-	check ( 'test@sdlkfjsdl.com' );
+	check ( 'test@andrethierry.com', 'relay-hosting.secureserver.net' );
 	check ( 'test@apple.com' );
-	check ( 'test@icloud.com' );
-	check ( 'test@me.com' );
-	check ( 'test@mac.com' );
-	check ( 'timmy@hushmail.com' );
-	check ('test@ryerson.ca');
-	check ('art@artegennaro.com');
-	check ( 'test@sendgrid.com' );
+	check ( 'test@artegennaro.com', 'relay-hosting.secureserver.net' );
+	check ( 'test@gmail.com', 'smtp.gmail.com' );
+	check ( 'test@hendriksandcregg.com' );
+	check ( 'test@hendriks.ca', 'smtp.gmail.com' );
+	check ( 'test@hotmail.com', 'smtp.live.com' );
+	check ( 'test@hushmail.com', 'smtp.hushmail.com' );
+	check ( 'test@ibm.com' );
+	check ( 'test@icloud.com', 'smtp.mail.me.com' );
+	check ( 'test@live.com', 'smtp.live.com' );
+	check ( 'test@mac.com', 'smtp.mail.me.com' );
+	check ( 'test@markoneill.com', 'smtp.bizmail.yahoo.com' );
+	check ( 'test@me.com', 'smtp.mail.me.com' );
+	check ( 'test@office365.com' );
+	check ( 'test@outlook.com', 'smtp.live.com' );
+	check ( 'test@rocketmail.com', 'plus.smtp.mail.yahoo.com' );
+	check ( 'test@ronwilsoninsurance.com', 'smtp.office365.com' );
+	check ( 'test@ryerson.ca' );
+	check ( 'test@sdlkfjsdl.com' );
+	check ( 'test@sdlkfjsdl.co.uk' );
+	check ( 'test@sdlkfjsdl.gov' );
+	check ( 'test@sdlkfjsdl.org' );
+	check ( 'test@sendgrid.com', 'smtp.gmail.com' );
+	check ( 'test@yahoo.ca', 'smtp.mail.yahoo.ca' );
+	check ( 'test@yahoo.co.id', 'smtp.mail.yahoo.co.id' );
+	check ( 'test@yahoo.co.in', 'smtp.mail.yahoo.co.in' );
+	check ( 'test@yahoo.co.kr', 'smtp.mail.yahoo.com' );
+	check ( 'test@yahoo.com', 'smtp.mail.yahoo.com' );
+	check ( 'test@yahoo.com.ar', 'smtp.mail.yahoo.com.ar' );
+	check ( 'test@yahoo.com.au', 'smtp.mail.yahoo.com.au' );
+	check ( 'test@yahoo.com.br', 'smtp.mail.yahoo.com.br' );
+	check ( 'test@yahoo.com.cn', 'smtp.mail.yahoo.com.cn' );
+	check ( 'test@yahoo.com.co' );
+	check ( 'test@yahoo.com.hk', 'smtp.mail.yahoo.com.hk' );
+	check ( 'test@yahoo.com.mx', 'smtp.mail.yahoo.com' );
+	check ( 'test@yahoo.com.my', 'smtp.mail.yahoo.com.my' );
+	check ( 'test@yahoo.com.ph', 'smtp.mail.yahoo.com.ph' );
+	check ( 'test@yahoo.com.sg', 'smtp.mail.yahoo.com.sg' );
+	check ( 'test@yahoo.com.tw', 'smtp.mail.yahoo.com.tw' );
+	check ( 'test@yahoo.com.vn', 'smtp.mail.yahoo.com.vn' );
+	check ( 'test@yahoo.co.nz', 'smtp.mail.yahoo.com.au' );
+	check ( 'test@yahoo.co.th', 'smtp.mail.yahoo.co.th' );
+	check ( 'test@yahoo.co.uk', 'smtp.mail.yahoo.co.uk' );
+	check ( 'test@yahoo.de', 'smtp.mail.yahoo.de' );
+	check ( 'test@yahoo.dk', 'smtp.mail.yahoo.com' );
+	check ( 'test@yahoo.es', 'smtp.correo.yahoo.es' );
+	check ( 'test@yahoo.fr', 'smtp.mail.yahoo.fr' );
+	check ( 'test@yahoo.ie', 'smtp.mail.yahoo.co.uk' );
+	check ( 'test@yahoo.it', 'smtp.mail.yahoo.it' );
+	check ( 'test@yahoo.no', 'smtp.mail.yahoo.com' );
+	check ( 'test@yahoo.pl', 'smtp.mail.yahoo.com' );
+	check ( 'test@yahoo.se', 'smtp.mail.yahoo.com' );
 }
-function check($email) {
+function check($email, $expectedSmtp = null) {
 	$d = new SmtpDiscovery ();
 	$smtp = $d->getSmtpServer ( $email );
-	if ($smtp) {
-		print $email . ' mx=' . $d->getPrimaryMx() . ' smtp=' . $smtp . "\n";
-	} else {
-		print $email . ' mx=' . $d->getPrimaryMx() . ' smtp=ASK USER' . "\n";
+	$displaySmtp = $smtp;
+	$displaySuccess = 'fail';
+	if ($smtp == $expectedSmtp) {
+		$displaySuccess = 'pass';
 	}
-}test ();
+	if (! $smtp) {
+		$displaySmtp = 'ASK USER';
+	}
+	print sprintf ( "%s: %s mx=%s smtp=%s\n", $displaySuccess, $email, $d->getPrimaryMx (), $displaySmtp );
+}
+test ();
