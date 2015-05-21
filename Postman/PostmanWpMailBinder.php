136792fae48f176de2ae8a46ef129781fc344e47
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 12:58:22 +0000

    Changed WHEN the binding warning occurs - right away, not after configuration.
    git-svn-id: https://plugins.svn.wordpress.org/postman-smtp/trunk@1164961 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Postman/PostmanWpMailBinder.php
+++ Postman/PostmanWpMailBinder.php
@@ -41,7 +41,11 @@ if (! class_exists ( "PostmanWpMailBinder" )) {
 				$binderOptions = PostmanOptions::getInstance ();
 				$binderAuthorizationToken = PostmanOAuthToken::getInstance ();
 				$ready = true;
-				if (! PostmanTransportRegistry::getInstance()->isPostmanReadyToSendEmail ( $binderOptions, $binderAuthorizationToken )) {
+				if (function_exists ( 'wp_mail' )) {
+					$this->logger->error ( 'wp_mail is already bound, Postman can not use it' );
+					$this->bindError = true;
+				}
+				if (! PostmanTransportRegistry::getInstance ()->isPostmanReadyToSendEmail ( $binderOptions, $binderAuthorizationToken )) {
 					$this->logger->debug ( 'Transport is not configured and ready' );
 					$ready = false;
 				}
@@ -49,7 +53,7 @@ if (! class_exists ( "PostmanWpMailBinder" )) {
 					$this->logger->error ( 'Prerequisite check failed' );
 					$ready = false;
 				}
-				if ($ready) {
+				if ($ready && ! $this->bindError) {
 					$this->logger->debug ( 'Binding to wp_mail()' );
 					$this->replacePluggableFunctionWpMail ();
 				}
@@ -68,34 +72,29 @@ if (! class_exists ( "PostmanWpMailBinder" )) {
 		 * @return boolean
 		 */
 		private function replacePluggableFunctionWpMail() {
-			if (! function_exists ( 'wp_mail' )) {
-				/**
-				 * The Postman drop-in replacement for the WordPress wp_mail() function
-				 *
-				 * @param string|array $to
-				 *        	Array or comma-separated list of email addresses to send message.
-				 * @param string $subject
-				 *        	Email subject
-				 * @param string $message
-				 *        	Message contents
-				 * @param string|array $headers
-				 *        	Optional. Additional headers.
-				 * @param string|array $attachments
-				 *        	Optional. Files to attach.
-				 * @return bool Whether the email contents were sent successfully.
-				 */
-				function wp_mail($to, $subject, $message, $headers = '', $attachments = array()) {
-					
-					// create an instance of PostmanWpMail to send the message
-					$postmanWpMail = new PostmanWpMail ();
-					return $postmanWpMail->send ( $to, $subject, $message, $headers, $attachments );
-				}
-				$this->logger->debug ( 'Bound to wp_mail()' );
-				$this->bound = true;
-			} else {
-				$this->logger->error ( 'Fatal Error: Tried to bind, but someone else beat us there' );
-				$this->bindError = true;
+			/**
+			 * The Postman drop-in replacement for the WordPress wp_mail() function
+			 *
+			 * @param string|array $to
+			 *        	Array or comma-separated list of email addresses to send message.
+			 * @param string $subject
+			 *        	Email subject
+			 * @param string $message
+			 *        	Message contents
+			 * @param string|array $headers
+			 *        	Optional. Additional headers.
+			 * @param string|array $attachments
+			 *        	Optional. Files to attach.
+			 * @return bool Whether the email contents were sent successfully.
+			 */
+			function wp_mail($to, $subject, $message, $headers = '', $attachments = array()) {
+				
+				// create an instance of PostmanWpMail to send the message
+				$postmanWpMail = new PostmanWpMail ();
+				return $postmanWpMail->send ( $to, $subject, $message, $headers, $attachments );
 			}
+			$this->logger->debug ( 'Bound to wp_mail()' );
+			$this->bound = true;
 		}
 		public function isBound() {
 			return $this->bound;
